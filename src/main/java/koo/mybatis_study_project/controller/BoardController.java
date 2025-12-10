045a11f2a712286732dc9905147321eccd78f17a
rngustj9139 <mnb9139@khu.ac.kr>
2025-12-10 20:45:19 +0900

    Add: BoardController's post save function

--- src/main/java/koo/mybatis_study_project/controller/BoardController.java
+++ src/main/java/koo/mybatis_study_project/controller/BoardController.java
@@ -1,12 +1,17 @@
 package koo.mybatis_study_project.controller;
 
+import koo.mybatis_study_project.dto.BoardDto;
 import koo.mybatis_study_project.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class BoardController {
 
     private final BoardService boardService;
@@ -14,6 +19,12 @@ public class BoardController {
     @GetMapping("/save")
     public String save() {
         return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDto boardDto) {
+        log.info("boardDto = {}", boardDto);
+        return "index";
     }
 
 }
