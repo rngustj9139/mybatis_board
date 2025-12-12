90fb0351c17af912852423abcb277e04943295de
rngustj9139 <mnb9139@khu.ac.kr>
2025-12-13 05:04:32 +0900

    Update: board-mapper

--- src/main/java/koo/mybatis_study_project/repository/BoardRepository.java
+++ src/main/java/koo/mybatis_study_project/repository/BoardRepository.java
@@ -12,6 +12,7 @@ public class BoardRepository {
     private final SqlSessionTemplate sqlSessionTemplate;
 
     public void save(BoardDto boardDto) {
+        // 테이블 생성 필요 (테이블에 데이터 insert)
         sqlSessionTemplate.insert("Board.save", boardDto); // Board는 board-mapper.xml의 Board를 의미, save는 쿼리문을 담고 있는 태그를 의미 (board-mapper.xml에 작성)
     }
 
