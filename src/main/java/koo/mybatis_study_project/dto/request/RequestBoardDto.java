525d88631405b4d13de4d50e236efa392126fff4
rngustj9139 <mnb9139@khu.ac.kr>
2025-12-14 15:53:30 +0900

    Update: RequestBoardDto

--- src/main/java/koo/mybatis_study_project/dto/request/RequestBoardDto.java
+++ src/main/java/koo/mybatis_study_project/dto/request/RequestBoardDto.java
@@ -1,5 +1,6 @@
 package koo.mybatis_study_project.dto.request;
 
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,9 +9,15 @@ import lombok.ToString;
 @Getter @Setter @ToString
 public class RequestBoardDto {
 
+    @NotNull(message = "작성자 이름은 Null일 수 없습니다.") // Null만 허용하지 않음, ""나 " "는 허용한다.
+    @Size(min = 1, max = 10, message = "작성자 이름은 1글자 이상 10글자 이하여야합니다.")
     private String boardWriter;
+    @NotEmpty // Null과 ""를 허용하지 않음, " "는 허용한다.
     private String boardPass; // 게시글 비밀번호
+    @NotBlank(message = "빈 값 입력 금지") // Null과 "", " "를 허용하지 않는다.
     private String boardTitle;
     private String boardContents;
+//    @Email // 이메일 형식이 아닌 경우 Exception 발생
+//    private String email;
 
 }
